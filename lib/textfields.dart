206a51ce4be34387d73fc7c46210dd4bc579d36c
zibertszn <alepon1337@mail.ru>
2025-11-14 22:55:23 +0300

    fix naming

--- lib/textfields.dart
+++ lib/textfields.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:dp/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPageButton extends StatelessWidget {
+class LoginPageText extends StatelessWidget {
   final String text;
   final TextEditingController controller;
   final bool obscure;
-  const LoginPageButton({
+  const LoginPageText({
     super.key,
     required this.text,
     required this.controller,
